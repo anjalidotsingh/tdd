251d9d34b758f709d7f74330d2c86ffd8d40e986
anjali <anjalidotsingh@gmail.com>
2022-04-14 12:13:34 +0530

    refractor getChange in vending machine

--- vendingmachine/getchange.go
+++ vendingmachine/getchange.go
@@ -9,26 +9,37 @@ import (
 var coins = []int{200, 100, 50, 20, 10, 5, 2, 1}
 
 func GetChange(totalPayable, cashPaid int) ([]int, error) {
-	var change []int
-	returnAmount := cashPaid - totalPayable
-	if returnAmount == 0 {
-		return change, nil
+	var returnCoins []int
+	difference := cashPaid - totalPayable
+	if difference == 0 {
+		return returnCoins, nil
 	}
-	if returnAmount < 0 {
-		return change, fmt.Errorf("amount paid is less than price of item")
+	if difference < 0 {
+		return returnCoins, fmt.Errorf("amount paid is less than price of item")
 	}
+	returnCoins=getReturnCoins(difference)
+	return returnCoins, nil
+}
+
+func appendCoinToReturnAmount(returnCoins []int,coin,noOfCoin int)[]int{
+	for i:=1;i<=noOfCoin;i++{
+		returnCoins = append(returnCoins, coin)
+	}
+	return returnCoins
+}
+
+func getReturnCoins(difference int)[]int{
+	var returnCoins []int
 	for _, coin := range coins {
-		if coin > returnAmount {
+		if coin > difference {
 			continue
 		}
-		noOfCoin:=int(math.Floor(float64(returnAmount)/float64(coin)))
-		for i:=1;i<=noOfCoin;i++{
-			change = append(change, coin)
-		}
-		returnAmount = returnAmount-(coin*noOfCoin)
-		if returnAmount == 0 {
+		noOfCoin:=int(math.Floor(float64(difference)/float64(coin)))
+		returnCoins=appendCoinToReturnAmount(returnCoins,coin,noOfCoin)
+		difference= difference-(coin*noOfCoin)
+		if difference == 0 {
 			break
 		}
 	}
-	return change, nil
+	return returnCoins
 }
