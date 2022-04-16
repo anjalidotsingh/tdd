f17f448651855835f16d6844f6f881ae85fc29b6
anjali <anjalidotsingh@gmail.com>
2022-04-16 22:27:29 +0530

    added exchange rate function for USD

--- multicurrencymoney/doller.go
+++ multicurrencymoney/doller.go
@@ -1,5 +1,11 @@
 package multicurrencymoney
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 type Bond struct {
 	Instrument string
 	Shares     int
@@ -12,4 +18,30 @@ func calculateTotalPrices(bonds []Bond) float64 {
 		totalBondsPrice += bond.Price * float64(bond.Shares)
 	}
 	return totalBondsPrice
+}
+
+func convertToUSD(currency string) (string, error) {
+	denomination, unit, _ := strings.Cut(currency, currencyDenonimationUnitSeparator())
+	denominationInFloat, _ := strconv.ParseFloat(denomination, 64)
+	exchangeRate := exchangeRateForUSD(unit)
+	if isExchangeRateNotAvailable(exchangeRate) {
+		return "", fmt.Errorf("exchange rate for %s is not available", unit)
+	}
+	return fmt.Sprintf("%s USD", strconv.FormatFloat(denominationInFloat*exchangeRate, 'f', -1, 64)),nil
+}
+
+func currencyDenonimationUnitSeparator() string {
+	return " "
+}
+
+func exchangeRateForUSD(currencyUnit string) float64 {
+	exchangeRateForUSDMap := map[string]float64{
+		"CHF": 1.5,
+		"INR": 0.0125,
+	}
+	return exchangeRateForUSDMap[currencyUnit]
+}
+
+func isExchangeRateNotAvailable(exchangeRate float64)bool{
+	return exchangeRate==0
 }
--- multicurrencymoney/doller_test.go
+++ multicurrencymoney/doller_test.go
@@ -6,9 +6,12 @@ import (
 
 /*
 to-do list
-1-add calculate price method 
-2-add exchange rate
-3-add support for multiple currency using exchange rate
+1-add calculate price method for USD
+2-add exchange rate to USD
+-for CHF
+-for other currency
+-for cuurency that doesnt exist in exchange table
+3-add calculate price support to USD for multiple currency using exchange rate
 */
 
 func TestCalculateTotalPrice(t *testing.T) {
@@ -27,7 +30,7 @@ func TestCalculateTotalPrice(t *testing.T) {
 		},
 		{
 			name:           "Instrument=IBM, Shares=1000, Price=25 and Instrument=GE, Shares=400, Price=100  gives TotalPrice=65000",
-			args:           args{[]Bond{{"IBM", 1000, 25},{"GE",400,100}}},
+			args:           args{[]Bond{{"IBM", 1000, 25}, {"GE", 400, 100}}},
 			wantTotalPrice: 65000,
 		},
 	}
@@ -35,6 +38,51 @@ func TestCalculateTotalPrice(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			if got := calculateTotalPrices(test.args.bonds); got != test.wantTotalPrice {
 				t.Errorf("got=%v want=%v", got, test.wantTotalPrice)
+			}
+		})
+	}
+}
+
+func TestConvertToUSD(t *testing.T) {
+	type args struct {
+		currency string
+	}
+	tests := []struct {
+		name      string
+		args      args
+		want      string
+		wantError bool
+	}{
+		{
+			name: "10 CHF to USD at rate 1.5 gives 15 USD",
+			args: args{"10 CHF"},
+			want: "15 USD",
+		},
+		{
+			name: "1 CHF to USD at rate 1.5 gives 1.5 USD",
+			args: args{"1 CHF"},
+			want: "1.5 USD",
+		},
+		{
+			name: "80 INR to USD at rate 0.0125 gives 1 USD",
+			args: args{"80 INR"},
+			want: "1 USD",
+		},
+		{
+			name:      "80 XYZ to USD when exchange rate are not available in table",
+			args:      args{"80 XYZ"},
+			want:      "",
+			wantError: true,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := convertToUSD(test.args.currency)
+			if got != test.want {
+				t.Errorf("got=%v want=%v", got, test.want)
+			}
+			if (err != nil) != test.wantError {
+				t.Errorf("got error %s", err.Error())
 			}
 		})
 	}
